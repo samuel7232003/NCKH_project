ff4137cb076c19b0876bc55e5c0545204bd506c7
Le Viet Thanh <115569080+samuel7232003@users.noreply.github.com>
2024-11-21 18:06:14 +0700

    [update] Grid view for diary

--- src/page/diarypage/DiaryPage.tsx
+++ src/page/diarypage/DiaryPage.tsx
@@ -17,9 +17,13 @@ import { Diary } from "../../redux/diary/diary.state";
 import { initalDiaryState } from "../../redux/diary/diary.slice";
 import InforChart from "../../components/infochart/InfoChart";
 import add_icon from './images/Add_round_duotone_fill_line.png'
-import { Tooltip } from "antd";
+import { DatePicker, Tooltip } from "antd";
 import AddDiaryBox from "../../components/diarypage/AddDiaryBox";
 import { useOutletContext } from "react-router-dom";
+import story_icon from './images/Stat.png';
+import grid_icon from './images/darhboard (1).png';
+import dayjs from "dayjs";
+import addDate_icon from './images/Add_ring_light.png'
 
 export default function DiaryPage(){
     const {setPage}:any = useOutletContext();
@@ -33,6 +37,8 @@ export default function DiaryPage(){
     const [index, setIndex] = useState(0);
     const [openBox, setOpenBox] = useState(false);
     const [openAdd, setOpenAdd] = useState(false);
+    const [mode, setMode] = useState(true);
+    const [time, setTime] = useState(dayjs());
 
     useEffect(()=>{
         setPage("diary");
@@ -80,6 +86,26 @@ export default function DiaryPage(){
         }
     }
 
+    function getListDate(day: dayjs.Dayjs){
+        const firstDay = dayjs(day.format("YYYY-MM") + "-01", 'YYYY-MM-DD');
+        const daysInMonth = firstDay.daysInMonth();
+        const daysList = [];
+        for (let day = 1; day <= daysInMonth; day++) {
+            daysList.push(firstDay.date(day).format('YYYY-MM-DD'));
+            if(firstDay.date(day).format('YYYY-MM-DD') === dayjs().format("YYYY-MM-DD")) day = 32;
+        }
+        return daysList;
+    }
+
+    function findDiary(date: string){
+        const find = listDiary.diarys.find(index => index.date === date);
+        if(find) return <DiaryBox account={account} diary={find}/>
+        else return <div className="dailybox pending">
+            <p className="date">{date}</p>
+            <figure onClick={() => setOpenAdd(true)}><img src={addDate_icon} alt="" /></figure>
+        </div>
+    }
+
     return(
         <main className="diarypage" style={{backgroundImage: `url(${getBackground(nowDiary.survey)}`}}>
             <div className="inner">
@@ -87,14 +113,28 @@ export default function DiaryPage(){
                     <figure><img src={tree_icon} alt="" /></figure>
                     <p>Thói quen ghi chú tâm trạng mỗi ngày giúp cho chúng ta có nhận thức hơn về việc quan tâm đến <b>Sức khỏe Tâm thần</b> của bản thân.  </p>
                     <figure><img src={tree_icon} alt="" /></figure>
+                    <div className='change-mode' onClick={() => setMode(!mode)}>
+                        <figure className='cal' style={mode ? {backgroundColor: "white"}: {}}><img src={story_icon} alt="" /></figure>
+                        <figure className='time' style={!mode ? {backgroundColor: "white"}: {}}><img src={grid_icon} alt="" /></figure>
+                    </div>
+                    {!mode&&<div className="pickDate">
+                        <DatePicker mode="month" picker="month" allowClear={false} value={time} onChange={(date) => setTime(date)} maxDate={dayjs()}/>
+                    </div>}
                 </div>
-                <div className="listDiary">
+                {mode?<div className="listDiary">
                     <div className="sub">{ backDiary && <DiaryBox account={account} diary={backDiary}/>}</div>
                     <figure onClick={() => setIndex(index-1)}>{ backDiary && <img src={back_icon} alt="" />}</figure>
                     <div className="main"><DiaryBox account={account} diary={nowDiary}/></div>
                     <figure onClick={() => setIndex(index+1)}>{ nextDiary && <img src={next_icon} alt="" />}</figure>
                     <div className="sub">{ nextDiary && <DiaryBox account={account} diary={nextDiary}/>}</div>
-                </div>
+                </div>:
+                <div className="gridDiary">
+                    <ul>{
+                        getListDate(time).map((index) => <li key={index}>
+                            {findDiary(index)}
+                        </li>)
+                    }</ul>
+                </div>}
                 {!openBox && <p className="see-chart" onClick={() => setOpenBox(true)}>Xem biểu đồ tâm trạng của bạn</p>}
             </div>
             {openBox && <div className="sub-box">
